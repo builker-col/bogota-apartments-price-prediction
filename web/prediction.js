bb978563f9cd395127a1504c3d87a5f08b287e1c
Zer0 <erik172dev@gmail.com>
2023-12-23 23:43:41 +0000

    Add HTML form and JavaScript for prediction feature

---
+++ web/prediction.js
@@ -0,0 +1,26 @@
+document.getElementById('predictionForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const formData = new FormData(this);
+
+    fetch('http://132.226.50.30:5501/chapinero/predict', {
+        method: 'POST',
+        body: JSON.stringify(Object.fromEntries(formData)),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            const formattedPrediction = formatNumberWithCommas(parseInt(data.prediction));
+            document.getElementById('predictionResult').innerText = `Precio de venta aproximado: $${formattedPrediction} COP`;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+});
+
+// Función para formatear el número con separadores de miles
+function formatNumberWithCommas(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
